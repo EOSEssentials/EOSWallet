73052a95b67f2847de25d91c86e7fc3dfee05fe8
Cesar Rodriguez <kesarr@gmail.com>
2018-02-16 00:52:55 +0100

    wip transfer

--- src/app/components/transfer/transfer.component.ts
+++ src/app/components/transfer/transfer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Renderer2} from '@angular/core';
+import {LocalStorage} from 'ngx-webstorage';
+import {Router} from '@angular/router';
+import * as Eos from 'eosjs';
 
 @Component({
   selector: 'app-transfer',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transfer.component.css']
 })
 export class TransferComponent implements OnInit {
+  @LocalStorage()
+  identity: any;
+  scatter: any = (<any>window).scatter; // TODO: move to a bigger scope as we will need it everywhere
 
-  constructor() { }
+  constructor(private router: Router, private renderer: Renderer2) {
+    renderer.listen('document', 'scatterLoaded', () => {
+        this.loadScatter();
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  loadScatter() {
+    this.scatter = (<any>window).scatter;
+    const network = {host: "testnet1.eos.io", port: 80};
+    const eosOptions = {};
+    this.scatter.eos(Eos.Testnet, network, eosOptions);
+
+  }
+
+  transfer() {
+    let that = this;
+    // TODO: transfer
+    return;
   }
 
 }
